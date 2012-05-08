dda93468afae23442a5fd0c87981a1c2fcd38959
Raghuveera Betina <rbetina@gmail.com>
2012-05-08 00:44:26 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendLinks::Application.config.secret_token = 'df0fee1f6d8bccd527af417936af13a35838037f8a81b5a2956ef9c6fa8aca9e775057c9848f1e3bef8b9d23dbeefc43e77106d128babd533266c81c25efe250'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FriendLinks::Application.config.session_store :cookie_store, key: '_FriendLinks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FriendLinks::Application.config.session_store :active_record_store
